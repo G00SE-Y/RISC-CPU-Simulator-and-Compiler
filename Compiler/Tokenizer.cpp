214e4ef4edcd2b992c441facabc08f53cf27b8e1
Jan D <79925808+jdarge@users.noreply.github.com>
2024-04-26 18:32:07 -0400

    Update Tokenizer.cpp

--- Compiler/Tokenizer.cpp
+++ Compiler/Tokenizer.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <regex>
 #include <sstream>
+#include <iterator>
 
 #include "Tokenizer.h"
 #include "RISCV32I_ISA.h"
@@ -9,14 +10,14 @@
 /*
 
 */
-std::vector<token> Tokenizer::tokenize_line(std::string line) {
+std::vector<token> Tokenizer::tokenize_line(const std::string& line) {
 
     std::vector<token> tokens;
     std::vector<std::string> symbols;
 
     // split line by whitespace and copy into symbols
     std::istringstream iss(line);
-    std::copy(std::istream_iterator<std::string>(iss), 
+    std::copy(std::istream_iterator<std::string>(iss),
         std::istream_iterator<std::string>(),
         std::back_inserter(symbols)
     );
@@ -32,7 +33,7 @@ std::vector<token> Tokenizer::tokenize_line(std::string line) {
 /*
 Takes a vector of strings in ASM and converts it into a vector of identified tokens
 */
-std::vector<std::vector<token>> Tokenizer::tokenize(std::vector<std::string> code) {
+std::vector<std::vector<token>> Tokenizer::tokenize(const std::vector<std::string>& code) {
 
     std::vector<std::vector<token>> tokens;
 
@@ -41,22 +42,22 @@ std::vector<std::vector<token>> Tokenizer::tokenize(std::vector<std::string> cod
         tokens.push_back(tokenize_line(line));
     }
 
-    
+    // TODO: missing return value
 }
 
 
 
-token Tokenizer::identify_token(std::string s) {
+token Tokenizer::identify_token(const std::string& s) {
 
     token t;
     
-    if((t.operation = is_op(s)) != BaseInt32_Instruction::NO_OP) { // if operation
+    if( (t.operation = is_op(s) ) != BaseInt32_Instruction::NO_OP) { // if operation
         t.type = token_type::OP;
     }
-    else if((t.value = is_reg(s)) != "") { // if register
+    else if( !(t.value = is_reg(s)).empty() ) { // if register
         t.type = token_type::REG;
     }
-    else if((t.value = is_imm(s)) != "") { // if immediate
+    else if( !(t.value = is_imm(s)).empty() ) { // if immediate
         t.type = token_type::IMM;
     }
 
@@ -64,7 +65,7 @@ token Tokenizer::identify_token(std::string s) {
 }
 
 
-BaseInt32_Instruction Tokenizer::is_op(std::string s) {
+BaseInt32_Instruction Tokenizer::is_op(const std::string& s) {
 
     try {
         return str_ins_BI32.at(s); // throws OOR exception if `s` isn't in the map
