50fbf214b6c5e34054e71d20ee7fa6f86ea42984
DonovanReynolds <donovanreynolds123@gmail.com>
2024-04-29 13:19:03 -0400

    Updated Runs.

--- CPU/CPU.srcs/sources_1/new/CPU.v
+++ CPU/CPU.srcs/sources_1/new/CPU.v
@@ -1,24 +1,7 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04/25/2024 05:59:51 PM
-// Design Name: 
-// Module Name: CPU
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
+/*
+    By: Donovan Reynolds
+*/
 
 module CPU(
 
