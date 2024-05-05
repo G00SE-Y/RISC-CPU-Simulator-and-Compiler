9d24e5a12714a5ea56c9621880726e01d05d8740
Ethan <bulldawg0065@gmail.com>
2024-05-04 20:17:35 -0400

    Added all rules for 32I into definitions map. Parsing cases need to be rewritten

--- Compiler/Parser.cpp
+++ Compiler/Parser.cpp
@@ -7,6 +7,18 @@
 #include "RISCV32I_ISA.h"
 #include "Tokenizer.h"
 
+#define ADD_NEW_OPERATOR(name, NAME, type)                                              \
+    Parser::definitions["name"] = Parser::Rule(                                         \
+    Parser::definition_type::TERM,                                                      \
+    Tokenizer::Token(Tokenizer::token_type::OPERATOR, BaseInt32_Instruction::NAME),     \
+        {}                                                                              \
+    );                                                                                  \
+    Parser::definitions["NAME_OP"] = Parser::Rule(                                      \
+        Parser::definition_type::DEF,                                                   \
+        Tokenizer::Token(),                                                             \
+        {"name", "TYPE_OPERANDS"}                                                                \
+    );                                                                                  \
+
 
 
 namespace Parser {
@@ -14,6 +26,8 @@ namespace Parser {
     std::unordered_map<std::string, Parser::Rule> definitions;
 
     void init_definitions();
+
+
 
     namespace {
 
@@ -118,34 +132,128 @@ namespace Parser {
 
     // This is not going to be pretty
     void init_definitions() {
-        Parser::definitions["add"] = Parser::Rule(
+
+        // used for program blocks
+        Parser::definitions["label"] = Parser::Rule(
             Parser::definition_type::TERM, 
-            Tokenizer::Token(Tokenizer::token_type::OPERATOR, BaseInt32_Instruction::ADD), 
+            Tokenizer::Token(Tokenizer::token_type::LABEL, BaseInt32_Instruction::NO_OP), 
             {}
         );
 
+        Parser::definitions["data"] = Parser::Rule(
+            Parser::definition_type::TERM, 
+            Tokenizer::Token(Tokenizer::token_type::DATA, BaseInt32_Instruction::NO_OP), 
+            {}
+        );
+
+        // used as operands
         Parser::definitions["register"] = Parser::Rule(
             Parser::definition_type::TERM, 
             Tokenizer::Token(Tokenizer::token_type::REGISTER, BaseInt32_Instruction::NO_OP), 
             {}
         );
 
-        Parser::definitions["ADD_OP"] = Parser::Rule(
-            Parser::definition_type::DEF, 
-            Tokenizer::Token(), 
-            {"add", "register", "register", "register"}
-        );
-
-        Parser::definitions["sub"] = Parser::Rule(
+        Parser::definitions["immediate"] = Parser::Rule(
             Parser::definition_type::TERM, 
-            Tokenizer::Token(Tokenizer::token_type::OPERATOR, BaseInt32_Instruction::SUB), 
+            Tokenizer::Token(Tokenizer::token_type::IMMEDIATE, BaseInt32_Instruction::NO_OP), 
             {}
         );
 
-        Parser::definitions["SUB_OP"] = Parser::Rule(
+
+        Parser::definitions["address"] = Parser::Rule(
+            Parser::definition_type::TERM, 
+            Tokenizer::Token(Tokenizer::token_type::ADDRESS, BaseInt32_Instruction::NO_OP), 
+            {}
+        );
+
+
+        // operand types
+        Parser::definitions["RTYPE_OPERANDS"] = Parser::Rule(
             Parser::definition_type::DEF, 
             Tokenizer::Token(), 
-            {"sub", "register", "register", "register"}
+            {"register", "register", "register"}
         );
+
+        Parser::definitions["ITYPE_OPERANDS"] = Parser::Rule(
+            Parser::definition_type::DEF, 
+            Tokenizer::Token(), 
+            {"register", "register", "immediate"}
+        );
+
+        Parser::definitions["STYPE_OPERANDS"] = Parser::Rule(
+            Parser::definition_type::DEF, 
+            Tokenizer::Token(), 
+            {"register", "register", "immediate"}
+        );
+
+        Parser::definitions["BTYPE_OPERANDS"] = Parser::Rule(
+            Parser::definition_type::DEF, 
+            Tokenizer::Token(), 
+            {"register", "register", "immediate"}
+        );
+
+        Parser::definitions["UTYPE_OPERANDS"] = Parser::Rule(
+            Parser::definition_type::DEF, 
+            Tokenizer::Token(), 
+            {"register", "immediate"}
+        );
+
+        Parser::definitions["JTYPE_OPERANDS"] = Parser::Rule(
+            Parser::definition_type::DEF, 
+            Tokenizer::Token(), 
+            {"register", "immediate"}
+        );
+
+        
+        // All the operations, I'm so sorry... it has to be done
+
+        Parser::definitions["nop"] = Parser::Rule(
+            Parser::definition_type::TERM,
+            Tokenizer::Token(Tokenizer::token_type::OPERATOR, BaseInt32_Instruction::NOP),
+                {}
+            );
+            Parser::definitions["NOP_OP"] = Parser::Rule(
+                Parser::definition_type::DEF,
+                Tokenizer::Token(),
+                {"nop"}
+        );
+
+        ADD_NEW_OPERATOR(add, ADD, R)
+        ADD_NEW_OPERATOR(addi, ADDI, I)
+        ADD_NEW_OPERATOR(and, AND, R)
+        ADD_NEW_OPERATOR(andi, ANDI, I)
+        ADD_NEW_OPERATOR(auipc, AUIPC, U)
+        ADD_NEW_OPERATOR(beq, BEQ, B)
+        ADD_NEW_OPERATOR(bge, BGE, B)
+        ADD_NEW_OPERATOR(bgeu, BGEU, B)
+        ADD_NEW_OPERATOR(blt, BLT, B)
+        ADD_NEW_OPERATOR(bltu, BLTU, B)
+        ADD_NEW_OPERATOR(bne, BNE, B)
+        ADD_NEW_OPERATOR(jal, JAL, J)
+        ADD_NEW_OPERATOR(jalr, JALR, I)
+        ADD_NEW_OPERATOR(lb, LB, I)
+        ADD_NEW_OPERATOR(lbu, LBU, I)
+        ADD_NEW_OPERATOR(lh, LH, I)
+        ADD_NEW_OPERATOR(lhu, LHU, I)
+        ADD_NEW_OPERATOR(lui, LUI, U)
+        ADD_NEW_OPERATOR(lw, LW, I)
+        ADD_NEW_OPERATOR(or, OR, R)
+        ADD_NEW_OPERATOR(ori, ORI, I)
+        ADD_NEW_OPERATOR(sb, SB, S)
+        ADD_NEW_OPERATOR(sh, SH, S)
+        ADD_NEW_OPERATOR(sll, SLL, R)
+        ADD_NEW_OPERATOR(slli, SLLI, R)
+        ADD_NEW_OPERATOR(slt, SLT, R)
+        ADD_NEW_OPERATOR(slti, SLTI, I)
+        ADD_NEW_OPERATOR(sltu, SLTU, R)
+        ADD_NEW_OPERATOR(sltiu, SLTIU, I)
+        ADD_NEW_OPERATOR(sra, SRA, R)
+        ADD_NEW_OPERATOR(srai, SRAI, R)
+        ADD_NEW_OPERATOR(srl, SRL, R)
+        ADD_NEW_OPERATOR(srli, SRLI, R)
+        ADD_NEW_OPERATOR(sub, SUB, R)
+        ADD_NEW_OPERATOR(sw, SW, S)
+        ADD_NEW_OPERATOR(xor, XOR, R)
+        ADD_NEW_OPERATOR(xori, XORI, I)
     }
 }
