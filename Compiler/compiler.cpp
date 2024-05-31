73295ce5141ef5696eef1bb9935b8043e9473c25
Ethan <bulldawg0065@gmail.com>
2024-05-31 18:57:15 -0400

    Generated code now outputs to a file given via command line argument and includes addresses (output is in hex)

--- Compiler/compiler.cpp
+++ Compiler/compiler.cpp
@@ -2,45 +2,59 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <bitset>
 
 #include "RISCV32I_ISA.h"
 #include "Tokenizer.h"
 #include "Parser.h"
 
-using namespace std;
+std::string bin_to_hex(std::string bin);
+
+using std::cout, std::endl;
 
 int main(int argc, char** argv) {
 
-    if(argc != 2) {
-        cout << "Fuck off!" << endl;
+    if(argc != 3) {
+        if(argc < 3) cout << "Not enough arguments!\n";
+        else cout << "Too many arguments!\n";
         return -1;
     }
 
-    string line;
-    vector<string> lines;
+    std::string line;
+    std::vector<std::string> lines;
 
-    ifstream fp(argv[1]);
-    if(fp.is_open()) {
-        while(getline(fp, line)) {
+    std::ifstream ifp(argv[1]);
+    std::ofstream ofp(argv[2]);
+
+    if(ifp.is_open() && ofp.is_open()) {
+        while(getline(ifp, line)) {
             cout << line << endl;
             lines.push_back(line);
         }
         cout << endl;
-        fp.close();
+        ifp.close();
         cout << "------------" << endl <<"Now tokenizing..." << endl;
         auto t = Tokenizer::tokenize(lines);
         cout << "------------" << endl <<"Now parsing and generating code..." << endl;
         auto code = Parser::parse(t);
 
-        if(code.is_error) {
+        if(code.error.size()) { // compilation error, do not write to file
             cout << "Error: " << endl;
             cout << code.error;
         }
-        else {
+        else { // no error, generate addresses and write generated code to file 
             cout << "Successfully generated code!" << endl;
-            cout << code.output << endl;
+
+            int address = 0;
+            for(auto& line: code.output) {
+
+                cout << std::bitset<32>(address).to_string() << "\t" << line << "\n";
+                ofp << "0x" << bin_to_hex(std::bitset<32>(address).to_string()) << "\t" << "0x" << bin_to_hex(line) << "\n";
+                address+=4;
+            }
 
         }
+        ofp.close();
 
     }
     else {
@@ -50,4 +64,29 @@ int main(int argc, char** argv) {
     cout << "\n\nSee ya!" << endl;
 
     return 0;
+}
+
+
+
+/*
+Converts a given binary string to a hex string without checking for chars //todo
+*/
+std::string bin_to_hex(std::string bin) {
+    
+    const char hex_digits[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+
+    if(bin.size() % 4 != 0) return "";
+
+    std::string hex = "";
+    int digit;
+
+    for(std::size_t i = 0; i < bin.size(); i += 4) {
+        
+        digit = std::stoi(bin.substr(i, 4), 0, 2);
+        // cout << bin.substr(i, 4) <<" -> " << hex_digits[digit] << "\n";
+
+        hex += hex_digits[digit];
+    }
+
+    return hex;
 }
