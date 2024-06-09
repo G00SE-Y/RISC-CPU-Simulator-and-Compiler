1c20f4e46858cc2dde2d1a08ec2d37b8136c3418
DonovanReynolds <donovanreynolds123@gmail.com>
2024-06-09 18:01:10 -0400

    Done with ALU operations used some minor test cases.

--- CPU/CPU.srcs/sim_1/new/testALU.v
+++ CPU/CPU.srcs/sim_1/new/testALU.v
@@ -9,6 +9,7 @@ module testALU();
 reg clk;
 reg [31:0]  source1;
 reg [31:0]  source2;
+reg [31:0] immediate;
 reg [31:0]  result;
 wire [2:0]  funct;
 wire [6:0]  funct7;
@@ -19,15 +20,17 @@ wire [31:0] resultConnection;
 
 
 initial clk = 0;
-initial source1 = -8;
-initial source2 = 1;
+initial source1 = 3;
+initial source2 = 5;
 initial result = 0;
 
-assign funct = 6;
-assign funct7 = 7'b0;
-assign opcode = 7'b0110011;
+initial immediate = 12'b011111111111;
 
-ALU alu (.result(resultConnection), .source1(source1), .source2(source2), .funct3(funct),.funct7(funct7),.opcode(opcode),.clk(clk));
+assign funct = 0;
+assign funct7 = 7'b0000000;
+assign opcode = 7'b0010011;
+
+ALU alu (.result(resultConnection), .source1(source1), .source2(source2),.immediate(immediate), .funct3(funct),.funct7(funct7),.opcode(opcode),.clk(clk));
 
 always @(posedge clk) begin
     result <= resultConnection;
--- CPU/CPU.srcs/sources_1/new/ALU.v
+++ CPU/CPU.srcs/sources_1/new/ALU.v
@@ -12,20 +12,26 @@
 
 */
 
-module ALU(result,source1,source2,funct3,funct7,opcode,clk);
+module ALU(result,source1,source2,immediate,funct3,funct7,opcode,clk);
 
 input [31:0]  source1;
 input [31:0]  source2;
+input [31:0] immediate;
 input [2:0]   funct3;
 input [6:0]   funct7;
 input [6:0]   opcode;
+
 input         clk;
 
 output [31:0] result;
 
 reg    [31:0] internalResult;
+reg    [31:0] signExtend;
 
 initial internalResult = 0;
+initial signExtend = 0;
+
+
 
 assign result = internalResult;
 
@@ -58,19 +64,65 @@ always @(posedge clk) begin
             8: internalResult <= source1 & source2;
             default: internalResult <= 0;
         endcase
+    end
 
     /*
     opcode b0010011 used for ADDI, SLLI, SLTI, SLTIU, XORI, SRLI, SRAI, ORI,  ANDI
     funct3:                  000,  001 , 010 , 011 ,  100 , 101 , 101 , 110 , 111
-
+    Needs sign extensions for anything unsigned.
     */
-    if opcode == 7'b0010011)begin
-        case(funct3)
-        0: internalResult <= immediate;
-        default: internalResult <= 0;
+    
+    if (opcode == 7'b0010011)begin
+        
+        if (immediate[11] == 1) signExtend <= 4294963200 + immediate;
+        else signExtend <= immediate;
+
+
+        case (funct3)
+            0: internalResult <= source1 + signExtend;
+            1: internalResult <= source1 << signExtend; 
+            2: begin 
+                if (source1[31] == 1 && signExtend[31] == 0) internalResult <= 1;
+                else if (source1[31] == 0 && signExtend[31] == 1) internalResult <= 0;
+                else internalResult <= (source1<signExtend) ? 1 : 0;
+            end
+            3: internalResult <= (source1<signExtend) ? 1 : 0; //By default verilog is unsigned here
+            4: internalResult <= (source1 ^ signExtend);
+            5: internalResult <= source1 >> signExtend[4:0];
+            6: begin
+                internalResult <= source1 >> signExtend[4:0];
+                if (source1[31] == 1) internalResult[31] <= 1;
+            end
+            7: internalResult <= source1 | signExtend;
+            8: internalResult <= source1 & signExtend;
+            default internalResult <= 0;
         endcase
     end
+
+/*  
+    opcode b1100011 used for BEQ, BNE, BLT, BGE, BLTU, BGEU
+    funct3                   0,   1,   4,   5,   6,    7
+*/
+    if (opcode == 7'b1100011)begin
+        case (funct3)
+        0: internalResult <= (source1 == source2) ? immediate : 0;
+        1: internalResult <= (source1 != source2) ? immediate : 0;
+        4: begin 
+            if (source1[31] == 1 && source2[31] == 0) internalResult <= immediate;
+            else if (source1[31] == 0 && source2[31] == 1) internalResult <= 0;
+            else internalResult <= (source1<source2) ? immediate : 0;
+        end
+        5: begin 
+            if (source1[31] == 1 && source2[31] == 0) internalResult <= 0;
+            else if (source1[31] == 0 && source2[31] == 1) internalResult <= immediate;
+            else internalResult <= (source1<source2) ? 0 : immediate;
+        end
+        6: internalResult <= (source1 < source2) ? immediate : 0;
+        7: internalResult <= (source1 < source2) ? 0 : immediate;
+        default internalResult <= 0;
+        endcase
     end
+    
 end
 
 
